89ded86c5ab55fe3c179e579ba8dd63220127f5e
wangshuodong <wawngshuodong2004@163.com>
2018-03-16 15:26:35 +0800

    wangsd

--- src/main/java/com/wangsd/web/controller/WechatController.java
+++ src/main/java/com/wangsd/web/controller/WechatController.java
@@ -17,19 +17,22 @@ public class WechatController {
 
     private static Logger log = LoggerFactory.getLogger(WeixinUtil.class);
 
+
+
     @RequestMapping("/index")
     public String index(String code, String state, Model model) throws IOException {
-
+        String appId = state;
         // 获取网页授权access_token
         WeixinOauth2Token weixinOauth2Token = WeixinUtil.getOauth2AccessToken("wxcfab4f09fe94c406", "adeb7e6a2142933188dc905e098af149", code);
         // 网页授权接口访问凭证
         String access_token = weixinOauth2Token.getAccess_token();
         // 用户标识
         String openid = weixinOauth2Token.getOpenid();
-
         log.info("openid=" + openid);
         // 获取用户信息
         //WechatUserInfo snsUserInfo = WeixinUtil.getWechatUserInfo(access_token, openid);
+
+
         return "wechat/index";
 
     }
